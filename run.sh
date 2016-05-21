6c61cd48ea7159837a8679e8c48b7c8cce0dc1e4
Gautier LOTERMAN <loterm_g@etna-alternance.net>
2016-05-21 02:27:36 +0200

    Add sourcefiles

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+JMETER_SLAVES=$(printenv | grep -o -P '(?<=1099_TCP_ADDR=).*' | awk '!x[$0]++' ORS=',' | awk '{gsub(/,$/,""); print}')
+
+jmeter.sh -n -Jclient.rmi.localport=60000 -Jclient.tries=3 -Jclient.retries_delay=10000 -Jclient.continue_on_fail=true -Djava.rmi.server.hostname=$HOSTNAME -R $JMETER_SLAVES "$@"
